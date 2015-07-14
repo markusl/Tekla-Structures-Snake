801ce644741bcc37bdab426a7a76bafb4339bdf0
Markus Lindqvist <markus.lindqvist@iki.fi>
2015-07-14 14:21:43 +0300

    Minor updates

--- Snake/SnakeGame.fs
+++ Snake/SnakeGame.fs
@@ -1,10 +1,6 @@
 ﻿module SnakeGame
 open System
 
-let snakeAreaSize = 20.
-let snakeOriginX, snakeOriginY = int snakeAreaSize / 2, int snakeAreaSize / 2
-let random = new Random()
-
 // Represents either a part of wall, snake or food
 type GameItem(X : int, Y : int, size : int) =
     member this.X = X
@@ -13,9 +9,13 @@ type GameItem(X : int, Y : int, size : int) =
     member this.size = size
     member this.eq (a: GameItem) = a.X = X && a.Y = Y
     
-let snakeStartPosition = [GameItem(snakeOriginX, snakeOriginY, 0);
-                          GameItem(snakeOriginX, snakeOriginY + 1, 0);
-                          GameItem(snakeOriginX, snakeOriginY + 2, 0)]
+
+let snakeAreaSize = 20.
+let private snakeOriginX, snakeOriginY = int snakeAreaSize / 2, int snakeAreaSize / 2
+let private random = new Random()
+let private snakeStartPosition = [GameItem(snakeOriginX, snakeOriginY, 0);
+                                  GameItem(snakeOriginX, snakeOriginY + 1, 0);
+                                  GameItem(snakeOriginX, snakeOriginY + 2, 0)]
 
 /// Where the snake is going?
 type SnakeDirection = Up | Down | Left | Right
@@ -31,13 +31,13 @@ type SnakeGame = {
     gameOver : bool;
 }
 
-let rec private randomize size except = begin
-    let randI() = random.Next(int snakeAreaSize) in
+let rec private createRandomElement size exclude =
+    let randI() = random.Next(int snakeAreaSize)
     let x, y = randI(), randI()
-    match List.tryFind (fun (e : GameItem) -> e.X = x && e.Y = y) except with 
-        | Some _ -> randomize size except
-        | _      -> GameItem(x, y, size)
-    end
+    if List.exists (fun (e : GameItem) -> e.X = x && e.Y = y) exclude then
+        createRandomElement size exclude
+    else
+        GameItem(x, y, size)
 
 /// Creates walls around the game area
 let private initWalls exclude =
@@ -49,12 +49,12 @@ let private initWalls exclude =
                    GameItem(i, int snakeAreaSize - 1, 0) :: borders
 
     for i in [0..5] do
-        borders <- randomize 0 (exclude @ borders) :: borders
+        borders <- createRandomElement 0 (exclude @ borders) :: borders
 
     borders
 
 let initFoods exclude =
-    [for i in {1..5} -> randomize 3 exclude]
+    [for i in {1..5} -> createRandomElement 3 exclude]
 
 let defaultWalls = initWalls snakeStartPosition
 
@@ -69,30 +69,28 @@ let initializeGame =
 let private growSnake (game : SnakeGame) =
     match game.snakeToGrown with 
         | 0 ->
-            let revbody = List.rev game.snake in
+            let revbody = List.rev game.snake
             { game with snake = revbody.Tail |> List.rev }
         | _ -> { game with snakeToGrown = game.snakeToGrown - 1 }
 
 let private maybeEatFood (game : SnakeGame) (head : GameItem) =
-    let (eated, survived) = List.partition head.eq game.foods in
+    let (eated, survived) = List.partition head.eq game.foods
     match eated with
         | h :: _ -> { game with snakeToGrown = game.snakeToGrown + h.size;
-                                foods = randomize 3 (game.snake @ game.walls) :: survived }
+                                foods = createRandomElement 3 (game.snake @ game.walls) :: survived }
         | [] -> game
 
 let snakeMove (game : SnakeGame) =
-    let game = growSnake game in
-    let old_head = game.snake.Head in
-    let new_head = match game.snakeDirection with 
-                    | Up    -> GameItem(old_head.X, old_head.Y - 1, 0)
-                    | Down  -> GameItem(old_head.X, old_head.Y + 1, 0)
-                    | Left  -> GameItem(old_head.X - 1, old_head.Y, 0)
-                    | Right -> GameItem(old_head.X + 1, old_head.Y, 0)
+    let game = growSnake game
+    let oldHead = game.snake.Head
+    let newHead = match game.snakeDirection with 
+                    | Up    -> GameItem(oldHead.X, oldHead.Y - 1, 0)
+                    | Down  -> GameItem(oldHead.X, oldHead.Y + 1, 0)
+                    | Left  -> GameItem(oldHead.X - 1, oldHead.Y, 0)
+                    | Right -> GameItem(oldHead.X + 1, oldHead.Y, 0)
 
-    let game = maybeEatFood game new_head
+    let game = maybeEatFood game newHead
 
-    let isHeadCrashing =  match game.snake @ game.walls |> List.tryFind new_head.eq with
-                            | Some _  -> false
-                            | None    -> true
-    { game with snake = new_head :: game.snake;
+    let isHeadCrashing =  game.snake @ game.walls |> List.exists newHead.eq
+    { game with snake = newHead :: game.snake;
                 gameOver = not isHeadCrashing }
--- Snake/TeklaSnake.fs
+++ Snake/TeklaSnake.fs
@@ -5,53 +5,55 @@
 ///
 module TeklaSnake
 open SnakeGame
-open Tekla.Structures.Model
+open Tekla.Structures
 open Tekla.Structures.Geometry3d
 
 /// At each update, all snake foods are recreated in Tekla Structures model
-let mutable private foods : Beam list = []
+let mutable private foods : Model.Beam list = []
 /// Keep track of maze objects. Maze is meant to be stable, so we only update
 /// possible changes to Tekla Structures model.
-let mutable private maze : (Beam * GameItem) list = []
+let mutable private maze : (Model.Beam * GameItem) list = []
 
 /// Definitions
 let private MAZE_MAX_Y = 30000.0
 let private GAME_SCALE = 1000.0
 let private MAZE_Z_LEVEL = 0.0
+let private FOOD_HEIGHT = 200.0
+let private MAZE_HEIGHT = 400.0
 
 let private greenClass = "3"
 let private redClass = "2"
 let private grayClass = "666"
-let private defaultMaterial = Material(MaterialString = "Steel_Undefined")
-let private defaultPosition = Position(Depth = Position.DepthEnum.MIDDLE)
-let private mazeProfile     = Profile(ProfileString = "PL600*600")
-let private snakeProfile    = Profile(ProfileString = "D500")
-let private foodProfile     = Profile(ProfileString = "D500")
-let private model : Model = new Model()
+let private defaultMaterial = Model.Material(MaterialString = "Steel_Undefined")
+let private defaultPosition = Model.Position(Depth = Model.Position.DepthEnum.MIDDLE)
+let private mazeProfile     = Model.Profile(ProfileString = "PL600*600")
+let private snakeProfile    = Model.Profile(ProfileString = "D500")
+let private foodProfile     = Model.Profile(ProfileString = "D500")
+let private model           = new Model.Model()
 
 /// Snake is represented by a polybeam with a round profile.
-let private snake : PolyBeam = new PolyBeam(Material = defaultMaterial, Position = defaultPosition,
-                                   Profile = snakeProfile, Class=greenClass)
+let private snake           = new Model.PolyBeam(Material = defaultMaterial, Position = defaultPosition,
+                                                 Profile = snakeProfile, Class=greenClass)
 
 /// Convert GameItem to Tekla Structures point
-let private extractPoints (item:GameItem) = new Point(float item.X * GAME_SCALE,
+let private extractPoints (item : GameItem) = new Point(float item.X * GAME_SCALE,
                                                       MAZE_MAX_Y - float item.Y * GAME_SCALE,
                                                       MAZE_Z_LEVEL)
 
-let private extractPointsZ z (item:GameItem) = new Point(float item.X * GAME_SCALE,
+let private extractPointsZ z (item : GameItem) = new Point(float item.X * GAME_SCALE,
                                                          MAZE_MAX_Y - float item.Y * GAME_SCALE,
                                                          z)
-let private contourPoints points = points |> List.map (fun e -> new ContourPoint(e, null))
+let private contourPoints points = points |> List.map (fun e -> new Model.ContourPoint(e, null))
 
-let private gameItemToPoints (items:GameItem list) = items |> List.map extractPoints |> contourPoints
+let private gameItemToPoints (items : GameItem list) = items |> List.map extractPoints |> contourPoints
 
-let private removeIfExists (teklaItem:Part) =
+let private removeIfExists (teklaItem : Model.Part) =
     if teklaItem.Identifier.IsValid() then
         teklaItem.Delete() |> ignore
 
-let private updateTeklaItem (teklaItem:PolyBeam) items =
+let private updateTeklaItem (teklaItem : Model.PolyBeam) items =
     removeIfExists teklaItem
-    teklaItem.Contour <- new Contour(ContourPoints=new System.Collections.ArrayList(items |> gameItemToPoints |> Array.ofList))
+    teklaItem.Contour <- new Model.Contour(ContourPoints=new System.Collections.ArrayList(items |> gameItemToPoints |> Array.ofList))
     teklaItem.Insert() |> ignore
     model.CommitChanges() |> ignore
 
@@ -67,7 +69,7 @@ let private createBeam color profile (startPoint, endPoint) =
 let private updateFoods (items:GameItem list) =
     foods |> List.map removeIfExists |> ignore
     let startAndEndPoints = List.zip (items |> List.map extractPoints)
-                                     (items |> List.map (extractPointsZ 200.0))
+                                     (items |> List.map (extractPointsZ FOOD_HEIGHT))
     foods <- startAndEndPoints |> List.map (createBeam redClass foodProfile)
 
 let private updateMaze (items:GameItem list) =
@@ -77,7 +79,7 @@ let private updateMaze (items:GameItem list) =
     removedItems |> List.map removeIfExists |> ignore
     let addedItems = items |> List.filter (fun item -> not (maze |> List.exists (fun i -> snd i = item)))
     let addedBeams = List.zip (addedItems |> List.map extractPoints)
-                              (addedItems |> List.map (extractPointsZ 200.0)) |> List.map (createBeam grayClass mazeProfile)
+                              (addedItems |> List.map (extractPointsZ MAZE_HEIGHT)) |> List.map (createBeam grayClass mazeProfile)
     let addedMaze = List.zip addedBeams addedItems
     maze <- itemsLeft @ addedMaze
 
